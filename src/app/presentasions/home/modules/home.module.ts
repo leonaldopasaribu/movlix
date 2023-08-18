dacc09a184c33b4da65458a7f2872c44c4ef2928
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-08-18 08:27:21 +0700

    feat: add header component

--- src/app/presentasions/home/modules/home.module.ts
+++ src/app/presentasions/home/modules/home.module.ts
@@ -4,8 +4,10 @@ import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from '../containers/home/home.component';
 
+import { HeaderModule } from 'src/app/shared/components/header/header.module';
+
 @NgModule({
   declarations: [HomeComponent],
-  imports: [HomeRoutingModule],
+  imports: [HomeRoutingModule, HeaderModule],
 })
 export class HomeModule {}
