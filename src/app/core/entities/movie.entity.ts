c874e535b784398816b1d9d2c5dc827bba8b8256
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-08-18 08:41:49 +0700

    feat: add movie entity

---
+++ src/app/core/entities/movie.entity.ts
@@ -0,0 +1,10 @@
+export interface MovieEntity {
+  backdropUrl: string;
+  id: number;
+  isAdult: boolean;
+  overview: string;
+  posterUrl: string;
+  rating: number;
+  releaseDate: string;
+  title: string;
+}
