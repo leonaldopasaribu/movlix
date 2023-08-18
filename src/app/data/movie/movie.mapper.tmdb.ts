59c5df717de4fbb89c8adcc039461959d9b57dac
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-08-18 10:27:37 +0700

    feat: add movie mapper tmdb

---
+++ src/app/data/movie/movie.mapper.tmdb.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+import { MovieDtoTmdb } from './movie.dto.tmdb';
+
+import { MovieEntity } from 'src/app/core/entities/movie.entity';
+
+import { EntityMapper } from 'src/app/shared/base/mapper';
+
+@Injectable()
+export class MovieMapperTmdb
+  implements EntityMapper<MovieDtoTmdb, MovieEntity>
+{
+  toEntity(dto: MovieDtoTmdb): MovieEntity {
+    return {
+      backdropUrl: dto.backdrop_path,
+      id: dto.id,
+      isAdult: dto.adult,
+      overview: dto.overview,
+      posterUrl: dto.poster_path,
+      rating: dto.vote_average,
+      releaseDate: dto.release_date,
+      title: dto.title,
+    };
+  }
+}
