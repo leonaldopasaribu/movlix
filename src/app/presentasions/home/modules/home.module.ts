4651e7ef1fe87d61d1ecbc4d4e3967889274a36f
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-08-18 14:09:27 +0700

    feat: add loading screen component

--- src/app/presentasions/home/modules/home.module.ts
+++ src/app/presentasions/home/modules/home.module.ts
@@ -3,18 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { HomeRoutingModule } from './home-routing.module';
 
+import { HeroComponent } from '../components/hero/hero.component';
 import { HomeComponent } from '../containers/home.component';
 import { MovieStore } from '../stores/movie.store';
 import { MovieViewModel } from '../view-models/movie.view-model';
 
 import { HeaderModule } from 'src/app/shared/components/header';
 import { CardListModule } from 'src/app/shared/components/card-list';
+import { LoadingScreenModule } from 'src/app/shared/components/loading-screen';
 
 import { MovieMapperTmdb } from 'src/app/data/movie/movie.mapper.tmdb';
 import { MovieRepositoryTmdb } from 'src/app/data/movie/movie.repository.tmdb';
 
 import { MovieRepository } from 'src/app/core/repositories/movie.repositories';
-import { HeroComponent } from '../components/hero/hero.component';
 
 @NgModule({
   declarations: [HomeComponent],
@@ -24,6 +25,7 @@ import { HeroComponent } from '../components/hero/hero.component';
     HeaderModule,
     CardListModule,
     HeroComponent,
+    LoadingScreenModule,
   ],
   providers: [
     MovieStore,
