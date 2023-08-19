4ec4e4adf668bf260f8a43577405ef3524785b9a
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-08-19 23:36:46 +0700

    refactor: use generic in local storage service

--- src/app/shared/services/local-storage/local-storage.service.ts
+++ src/app/shared/services/local-storage/local-storage.service.ts
@@ -4,12 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LocalStorageService {
-  setItem(key: string, value: unknown): void {
+  setItem<T>(key: string, value: T): void {
     localStorage.setItem(key, JSON.stringify(value));
   }
 
-  getItem(key: string): unknown {
+  getItem<T>(key: string): T | null {
     const value = localStorage.getItem(key);
+
     return value ? JSON.parse(value) : null;
   }
 
