51a4c4fcebc168e607c73117b21feb5b2c306f22
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-08-19 09:21:55 +0700

    test: add unit test for card component

---
+++ src/app/shared/components/card/card.component.spec.ts
@@ -0,0 +1,40 @@
+/* tslint:disable:no-unused-variable */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+import { CardComponent } from './card.component';
+
+describe('CardComponent', () => {
+  let component: CardComponent;
+  let fixture: ComponentFixture<CardComponent>;
+  let debugElement: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CardComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+  });
+
+  it('should create CardComponent', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should emit clicked event when card is clicked', () => {
+    spyOn(component.clicked, 'emit');
+
+    fixture.detectChanges();
+
+    const containerCard = debugElement.query(By.css('#CntrCard'));
+
+    containerCard.triggerEventHandler('click', null);
+
+    expect(component.clicked.emit).toHaveBeenCalled();
+  });
+});
