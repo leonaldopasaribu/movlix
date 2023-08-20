958fb4124281b555927fbd8a8dc79d89bf157b2d
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-08-20 21:25:30 +0700

    test: add unit test for movie mapper tmdb

---
+++ src/app/data/movie/movie.mapper.tmdb.spec.ts
@@ -0,0 +1,62 @@
+import { TestBed } from '@angular/core/testing';
+
+import { MovieMapperTmdb } from './movie.mapper.tmdb';
+import { MovieDtoTmdb } from './movie.dto.tmdb';
+
+import { MovieEntity } from 'src/app/core/entities/movie.entity';
+
+describe('MovieMapperTmdb', () => {
+  let mapper: MovieMapperTmdb;
+
+  const entityStub: MovieEntity = {
+    backdropUrl: 'Test Backdrop Url',
+    duration: 120,
+    genre: [{ id: 1, name: 'Comedy' }],
+    id: 1,
+    isAdult: false,
+    overview: 'Test Overview',
+    posterUrl: 'Test Poster Url',
+    rating: 8,
+    releaseDate: '2023-10-12',
+    title: 'Test Title',
+  };
+
+  const dtoStub: MovieDtoTmdb = {
+    adult: false,
+    backdrop_path: 'Test Backdrop Url',
+    genres: [{ id: 1, name: 'Comedy' }],
+    genre_ids: [1],
+    id: 1,
+    original_language: 'en',
+    original_title: 'Test Original Title',
+    overview: 'Test Overview',
+    popularity: 12345,
+    poster_path: 'Test Poster Url',
+    release_date: '2023-10-12',
+    runtime: 120,
+    title: 'Test Title',
+    video: false,
+    vote_average: 8,
+    vote_count: 1_000,
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MovieMapperTmdb],
+    });
+  });
+
+  beforeEach(() => {
+    mapper = TestBed.inject(MovieMapperTmdb);
+  });
+
+  it('should create MovieMapperTmdb', () => {
+    expect(mapper).toBeTruthy();
+  });
+
+  it('should transform MovieDtoTmdb to MovieEntity', () => {
+    const result = mapper.toEntity(dtoStub);
+
+    expect(result).toEqual(entityStub);
+  });
+});
