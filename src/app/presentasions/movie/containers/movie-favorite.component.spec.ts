b88ce41588a84fa614f43a2d3387eb88230f557e
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-08-21 19:16:31 +0700

    test: add unit test for movie favorite component

---
+++ src/app/presentasions/movie/containers/movie-favorite.component.spec.ts
@@ -0,0 +1,59 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovieFavoriteComponent } from './movie-favorite.component';
+
+import { CardListComponent } from '../components/card-list.component';
+import { MovieFavoriteViewModel } from '../view-models/movie-favorite.view-model';
+
+import { HeaderModule } from 'src/app/shared/components/header/header.module';
+import { DialogModule } from 'src/app/shared/components/dialog/dialog.module';
+
+describe('MovieFavoriteComponent', () => {
+  let component: MovieFavoriteComponent;
+  let fixture: ComponentFixture<MovieFavoriteComponent>;
+  let viewModel: MovieFavoriteViewModel;
+
+  const movieFavoriteViewModelSpy = jasmine.createSpyObj(
+    'MovieFavoriteViewModel',
+    ['getFavoriteMovies'],
+  );
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MovieFavoriteComponent, CardListComponent],
+      imports: [HeaderModule, DialogModule],
+      providers: [
+        {
+          provide: MovieFavoriteViewModel,
+          useValue: movieFavoriteViewModelSpy,
+        },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MovieFavoriteComponent);
+    component = fixture.componentInstance;
+    viewModel = TestBed.inject(MovieFavoriteViewModel);
+  });
+
+  it('should create MovieFavoriteComponent', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should return 1 when trackByIndex is called with argument 1', () => {
+    const stubArgument = 1;
+
+    const result = component.trackByIndex(1);
+
+    expect(result).toEqual(stubArgument);
+  });
+
+  it('should call getFavoriteMovies from movie detail view model when component executed', () => {
+    const getFavoriteMoviesSpy = viewModel.getFavoriteMovies as jasmine.Spy;
+
+    fixture.detectChanges();
+
+    expect(getFavoriteMoviesSpy).toHaveBeenCalled();
+  });
+});
