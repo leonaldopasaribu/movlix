9f34c752e1a91548feb983986c7ed45e590556c8
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2026-01-26 13:07:30 +0700

    feat: integrate @vercel/analytics for enhanced tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'movlix';
+
+  ngOnInit(): void {
+    inject();
+  }
 }
